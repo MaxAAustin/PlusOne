c7375e1070c1d114c83f84e6f2db2005c6570a1c
michael_austin <maxaustin92@gmail.com>
2017-10-25 17:03:03 -0400

    initial commit

---
+++ app/src/main/java/com/maxaaustin/plusone/MainActivity.java
@@ -0,0 +1,26 @@
+package com.maxaaustin.plusone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button1 = findViewById(com.maxaaustin.plusone.R.id.buttonOne);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent map = new Intent(MainActivity.this, GuestMap.class);
+                startActivity(map);
+            }
+        });
+    }
+}
