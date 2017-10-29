542a2700a7ea002e34c725d960ae605fdc818df8
Michael Austin <maxaustin92@gmail.com>
2017-10-29 08:31:26 -0400

    Added HostMain class and layout, plus style attributes and a dummy food view.
    Added a Popup menu with menu resource.

--- app/src/main/java/com/maxaaustin/plusone/HostMain.java
+++ app/src/main/java/com/maxaaustin/plusone/HostMain.java
@@ -1,13 +1,49 @@
 package com.maxaaustin.plusone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class HostMain extends AppCompatActivity {
+
+    private Button mHostDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_main);
+
+        mHostDetail = (Button) findViewById(R.id.hostDetailID);
+        mHostDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu mPopUpMenu = new PopupMenu(HostMain.this, view);
+                mPopUpMenu.getMenuInflater().inflate(R.menu.host_popup_menu, mPopUpMenu.getMenu());
+
+                mPopUpMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(HostMain.this, "You clicked: " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                mPopUpMenu.show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.host_popup_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/maxaaustin/plusone/MainActivity.java
+++ app/src/main/java/com/maxaaustin/plusone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.maxaaustin.plusone;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,22 +8,43 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button1 = findViewById(com.maxaaustin.plusone.R.id.buttonOne);
+        addListenerGuest();
+        addListenerHost();
+
+    }
+
+    public void addListenerGuest(){
+        final Context context = this;
+
+        Button button1 = findViewById(com.maxaaustin.plusone.R.id.buttonOne);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent map = new Intent(MainActivity.this, GuestMap.class);
+                Intent map = new Intent(context, GuestMap.class);
                 startActivity(map);
 
-                Intent host = new android.content.Intent(MainActivity.this, HostMain.class);
+
             }
         });
     }
+
+    public void addListenerHost(){
+        final Context context = this;
+
+        Button button1 = findViewById(R.id.buttonTwo);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent map = new Intent(context, HostMain.class);
+                startActivity(map);
+            }
+        });
+    }
+
 }
