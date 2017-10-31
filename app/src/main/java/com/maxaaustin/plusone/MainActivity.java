0131896cec28e6059063bee07f7aad80e444faf4
michael_austin <maxaustin92@gmail.com>
2017-10-30 22:01:57 -0400

    Added GridLayout with RV for cuisine types

--- app/src/main/java/com/maxaaustin/plusone/MainActivity.java
+++ app/src/main/java/com/maxaaustin/plusone/MainActivity.java
@@ -4,10 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    GridLayoutManager mGridLayoutManager;
+
+    String[] nameList = {
+            "American", "Barbecue", "Chinese", "French", "Indian", "Italian", "Japanese", "Mediterranean"
+    };
+    int[] cuisineIconID = {
+            R.drawable.american, R.drawable.bbq, R.drawable.chinese, R.drawable.french, R.drawable.indian, R.drawable.italian, R.drawable.japanese3, R.drawable.mediterranean
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent map = new Intent(context, GuestMap.class);
                 startActivity(map);
-
-
             }
         });
     }
@@ -41,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent map = new Intent(context, HostMain.class);
-                startActivity(map);
+                Intent host = new Intent(getApplicationContext(), HostMain.class);
+                startActivity(host);
             }
         });
     }
